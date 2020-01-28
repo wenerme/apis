66194723ccbd4d0e754a5a96f48ece6c0a7e3ff8
wener <wenermail@gmail.com>
2020-01-28 23:39:18 +0800

    log sqlite version

--- db/connection.ts
+++ db/connection.ts
@@ -4,13 +4,22 @@ import {Connection} from 'typeorm/connection/Connection';
 import {PhoneDataIndexEntity, PhoneDataRecordEntity} from 'libs/phonedata/schema';
 import {SqlAr} from './entity/SqlAr';
 // tell zeit/now include sqlite
-import 'sqlite3'
+import {Database} from 'sqlite3'
 
 let _connection: Connection;
 
 export function createDatabaseConnection() {
   if (_connection) {
     return _connection;
+  }
+  {
+    const db = new Database(':memory:');
+    db.serialize(() => {
+      db.each('select sqlite_version() as version', (err, row) => {
+        console.log(`sqlite version ${row.version}`)
+      });
+    });
+    db.close();
   }
   return createConnection({
     type: 'sqlite',
