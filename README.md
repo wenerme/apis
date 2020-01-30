3ad590e46e14c9928448ccc2a71b2de7966517d6
wener <wenermail@gmail.com>
2020-01-30 23:15:00 +0800

    1. 修复自定义服务 2. 完善打包说明

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Wener's APIs
+
+## 开发
+* 使用 Node 12 LTS 版本
+
+```bash
+# 如果使用 nvm
+nvm install 12
+nvm use 12
+```
+
+## Bundle
+
+```bash
+# ncc 构建单文件
+ncc build scripts/setup-db.ts
+# 执行
+node dist/index.js
+
+# pkg 打包 next 应用
+yarn build
+pkg -t node12-macos-x64 . --out-dir dist
+```
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const {createServer} = require('http');
 const {parse} = require('url');
 const next = require('next');
+const path = require('path');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev, dir: __dirname});
+const app = next({dev, dir: path.join(__dirname, '..')});
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
