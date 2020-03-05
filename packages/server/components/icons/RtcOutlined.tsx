3ecb60cce47e4bc7d6257feb1167789d42ea082f
wener <wenermail@gmail.com>
2020-03-05 21:50:14 +0800

    add missing icons

---
+++ packages/server/components/icons/RtcOutlined.tsx
@@ -0,0 +1,13 @@
+import React, {ForwardRefRenderFunction} from 'react';
+import RtcOutlinedSvg from '../../public/icons/svg/rtc-outlined.svg'
+import Icon, {IconComponentProps} from '@ant-design/icons/lib/components/Icon';
+
+const RtcOutlined: ForwardRefRenderFunction<any, IconComponentProps> = (props, ref) => {
+  return React.createElement(Icon, Object.assign({}, props, {
+    ref,
+    component: RtcOutlinedSvg
+  }));
+};
+
+RtcOutlined.displayName = 'RtcOutlined';
+export default React.forwardRef(RtcOutlined);
