b8c767aec6caf4c6d34f29ffc86d14309cbc9dbc
wener <wenermail@gmail.com>
2020-03-10 02:30:44 +0800

    add custom _error.js

---
+++ packages/server/pages/_error.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Error from 'next/error';
+
+export default class PageError extends Error {
+  render() {
+    return <div>Page error {this.props.statusCode} {this.props.title} </div>
+  }
+}
