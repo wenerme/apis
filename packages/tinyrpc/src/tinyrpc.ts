c23309f07548f70c2f07fc7fb188577f16ee04ad
wener <wenermail@gmail.com>
2020-03-25 20:56:06 +0800

    setup rollup for tinyrpc

--- packages/tinyrpc/src/tinyrpc.ts
+++ packages/tinyrpc/src/tinyrpc.ts
@@ -2,3 +2,5 @@ export { GlobalRegistry } from './global';
 export * from './types';
 export * from './ServiceRegistry';
 export * from './consumer';
+
+export { PingService } from './services/PingService';
