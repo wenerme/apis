6a1d678d5f6c90d32b78503d10d57c040cee2fa1
wener <wenermail@gmail.com>
2020-03-27 23:07:21 +0800

    add BarsSpinner.tsx

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../stories/**/*.stories.(js|ts|tsx|mdx)', '../src/**/*.stories.(ts|tsx)'],
+  stories: ['../stories/**/*.stories.(js|ts|tsx|mdx)', '../src/**/*.stories.(js|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
