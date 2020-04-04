59f40a9ea07ff4fbcac45b2eb26648b2a0f55090
wener <wenermail@gmail.com>
2020-04-04 18:04:54 +0800

    BoxShuffle support title and detail

--- packages/ui/src/loaders/BoxShuffle.stories.tsx
+++ packages/ui/src/loaders/BoxShuffle.stories.tsx
@@ -1,13 +1,14 @@
 import centered from '@storybook/addon-centered/react';
 import React from 'react';
 import { BoxShuffle } from 'src/loaders/BoxShuffle';
+import { text, withKnobs } from '@storybook/addon-knobs';
 
 export default {
   title: 'loader/spinner/BoxShuffle',
   component: BoxShuffle,
   decorators: [
     (storyFn) => (
-      <div style={{ backgroundColor: '#455a64', height: 400, width: 300, position: 'relative' }}>
+      <div style={{ backgroundColor: 'white', height: 400, width: 300, position: 'relative' }}>
         <div style={{ textAlign: 'center', position: 'absolute', top: 50, left: 0, right: 0 }}>
           <a href="https://codepen.io/golle404/pen/EKOoyW" target="_blank" rel="noreferrer noopener">
             CodePen
@@ -16,8 +17,12 @@ export default {
         <div>{storyFn()}</div>
       </div>
     ),
-    centered
-  ]
+    centered,
+    withKnobs,
+  ],
 };
 
 export const BasicBoxShuffle = BoxShuffle;
+export const TitledBoxShuffle = () => {
+  return <BoxShuffle title={text('Title', 'Loadding...')} detail={text('Detail', 'src/test/Component.tsx')} />;
+};
--- packages/ui/src/loaders/BoxShuffle.tsx
+++ packages/ui/src/loaders/BoxShuffle.tsx
@@ -8,31 +8,28 @@ top: 50%;
 left: 50%;
 transform: translate(-50%, -50%);
 `;
-const fbox = (props: any) => `
-  ${cbox}
-  width: 100%;
-  height: 100%;
-`;
 
-const BoxShuffleContainer = styled.div<{ size?: string,colors?:string[] }>`
+const BoxShuffleContainer = styled.div<{ size?: string; colors?: string[] }>`
   --size: ${(props) => props['size'] || '3em'};
 
-  ${cbox};
-  transform-style: preserve-3d;
-  perspective: 2000px;
-  transform: rotateX(-30deg) rotateY(-45deg);
+  .container {
+    ${cbox};
+    transform-style: preserve-3d;
+    perspective: 2000px;
+    transform: rotateX(-30deg) rotateY(-45deg);
 
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
+    *,
+    *:before,
+    *:after {
+      box-sizing: border-box;
+    }
   }
 
   .holder {
     ${cbox};
     transform-style: preserve-3d;
     transform: translate3d(0em, var(--size), calc(var(--size) * 0.5));
-    &:last-child {
+    &:nth-child(3) {
       transform: rotateY(-90deg) rotateX(90deg) translate3d(0, var(--size), calc(var(--size) * 0.5));
     }
     &:first-child {
@@ -41,8 +38,8 @@ const BoxShuffleContainer = styled.div<{ size?: string,colors?:string[] }>`
   }
 
   ${(props) =>
-  (props.colors || ['#1FBCD3', '#CBE2B4', '#F6B6CA']).map(
-    (color, i) => `
+    (props.colors || ['#1FBCD3', '#CBE2B4', '#F6B6CA']).map(
+      (color, i) => `
     .holder:nth-child(${i + 1}){
       .box{
         background-color: ${color};
@@ -55,8 +52,26 @@ const BoxShuffleContainer = styled.div<{ size?: string,colors?:string[] }>`
       }
     }
   }
-`
-  )}
+`,
+    )}
+
+  .info {
+    ${cbox};
+    padding-top: 180px;
+
+    .title {
+      font-size: 20px;
+      font-weight: 400;
+      text-align: center;
+      color: #212121;
+    }
+
+    .detail {
+      font-size: 14px;
+      font-weight: 200;
+      text-align: center;
+    }
+  }
 
   .box {
     ${cbox};
@@ -126,126 +141,24 @@ const BoxShuffleContainer = styled.div<{ size?: string,colors?:string[] }>`
   }
 `;
 
-/*
-$s: 3em;
-$colors: #1FBCD3, #CBE2B4, #F6B6CA;
-
-*, *:before, *:after{
-	box-sizing: border-box;
-}
-body{
-	background: #455a64;
-}
-.container{
-	@include cbox;
-	transform-style: preserve-3d;
-	perspective: 2000px;
-	transform: rotateX(-30deg) rotateY(-45deg);
-}
-.holder{
-	@include cbox;
-	transform-style: preserve-3d;
-	transform: translate3d(0em, $s, $s*.5);
-	&:last-child{
-		transform: rotateY(-90deg) rotateX(90deg) translate3d(0, $s, $s*.5);
-	}
-	&:first-child{
-		transform: rotateZ(-90deg) rotateX(-90deg) translate3d(0, $s, $s*.5);
-	}
-	@for $i from 1 through 3{
-		$color: nth($colors,$i);
-		&:nth-child(#{$i}){
-			.box{
-				background-color: $color;
-				&:before{
-					background-color: darken($color,20);
-				}
-				&:after{
-					background-color: darken($color,10);
-				}
-			}
-		}
-	}
-}
-.box{
-	@include cbox;
-	transform-style: preserve-3d;
-	animation: ani-box 6s infinite;
-	width: $s;
-	height: $s;
-	//opacity: .9;
-	&:before, &:after{
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		content: "";
-	}
-	&:before{
-		left: 100%;
-		bottom: 0;
-		transform: rotateY(90deg);
-		transform-origin: 0 50%;
-	}
-	&:after{
-		left: 0;
-		bottom: 100%;
-		transform: rotateX(90deg);
-		transform-origin: 0 100%;
-	}
-}
-
-@keyframes ani-box{
-	8.33%{
-		transform: translate3d(-50%,-50%,0) scaleZ(2);
-	}
-	16.7%{
-		transform: translate3d(-50%,-50%,-$s) scaleZ(1);
-	}
-	25%{
-		transform: translate3d(-50%,-100%,-$s) scaleY(2);
-	}
-	33.3%{
-		transform: translate3d(-50%,-150%,-$s) scaleY(1);
-	}
-	41.7%{
-		transform: translate3d(-100%,-150%,-$s) scaleX(2);
-	}
-	50%{
-		transform: translate3d(-150%,-150%,-$s) scaleX(1);
-	}
-	58.3%{
-		transform: translate3d(-150%,-150%, 0) scaleZ(2);
-	}
-	66.7%{
-		transform: translate3d(-150%,-150%,0) scaleZ(1);
-	}
-	75%{
-		transform: translate3d(-150%,-100%,0) scaleY(2);
-	}
-	83.3%{
-		transform: translate3d(-150%,-50%,0) scaleY(1);
-	}
-	91.7%{
-		transform: translate3d(-100%,-50%,0) scaleX(2);
-	}
-	100%{
-		transform: translate3d(-50%,-50%,0) scaleX(1);
-	}
-}
-
- */
-
-export const BoxShuffle: React.FC<{ size?: string }> = () => {
+export const BoxShuffle: React.FC<{ size?: string; title?: React.ReactNode; detail?: React.ReactNode }> = (props) => {
+  const { title, detail } = props;
   return (
     <BoxShuffleContainer>
-      <div className="holder">
-        <div className="box" />
+      <div className="container">
+        <div className="holder">
+          <div className="box" />
+        </div>
+        <div className="holder">
+          <div className="box" />
+        </div>
+        <div className="holder">
+          <div className="box" />
+        </div>
       </div>
-      <div className="holder">
-        <div className="box" />
-      </div>
-      <div className="holder">
-        <div className="box" />
+      <div className="info">
+        <div className="title">{title}</div>
+        <div className="detail">{detail}</div>
       </div>
     </BoxShuffleContainer>
   );
