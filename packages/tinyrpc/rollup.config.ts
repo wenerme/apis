330836698c08090088898913aebb7a5d93e51011
wener <wenermail@gmail.com>
2020-04-05 01:08:14 +0800

    tweaking rollup.config.ts

--- packages/tinyrpc/rollup.config.ts
+++ packages/tinyrpc/rollup.config.ts
@@ -1,16 +1,18 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import camelCase from 'lodash/camelcase';
-import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
-// rollup-plugin-node-resolve rollup-plugin-commonjs rollup-plugin-sourcemaps lodash.camelcase rollup-plugin-typescript2 rollup-plugin-json
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+// import sourceMaps from 'rollup-plugin-sourcemaps';
+import camelCase from 'lodash.camelcase';
+import json from '@rollup/plugin-json';
+// yarn add -D rollup @rollup/plugin-{commonjs,node-resolve,typescript,json} tslib typescript
 const pkg = require('./package.json');
 
 const libraryName = 'tinyrpc';
 
 export default {
-  input: `src/${libraryName}.ts`,
+  // ts not working
+  // https://github.com/rollup/plugins/issues?q=is%3Aissue+is%3Aopen+label%3A%22%F0%9F%94%8C+plugin-typescript%22+
+  // input: `src/${libraryName}.ts`,
+  input: `lib/${libraryName}.js`,
   output: [
     { file: pkg.browser, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
@@ -21,18 +23,12 @@ export default {
     include: 'src/**',
   },
   plugins: [
-    // Allow json resolution
     json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true,tsconfig:'tsconfig.rollup.json' }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    // typescript({ tsconfig: 'tsconfig.rollup.json'}),
+    commonjs({ extensions: ['.js', '.ts', '.tsx'] }),
     resolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
+    // sourceMaps(),
   ],
 };
