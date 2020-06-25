7fb4ff6ab5555f7513e08c9b97be5fab5bb8541d
wener <wenermail@gmail.com>
2020-06-25 15:03:47 +0800

    fix yarn2 build utils

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,1 +1,10 @@
-export * from './lib';
+"use strict";
+
+exports.__esModule = true;
+
+var _src = require("./src");
+
+Object.keys(_src).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  exports[key] = _src[key];
+});
