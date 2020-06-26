3f4abd8782129ebae54bd01ff121559b201c687a
wener <wenermail@gmail.com>
2020-06-26 16:54:24 +0800

    manually create dir

--- build/build-apis.sh
+++ build/build-apis.sh
@@ -17,6 +17,7 @@ mkdir -p packages/server/public/storybook
 
 for i in ui tinyrpc utils rjsf-antd-theme; do
   name=$(jq '.name' -r packages/$i/package.json)
-  [ -d "packages/$i/docs" ] && rsync -aR packages/$i/docs/ packages/server/public/pkgs/$name/docs/
-  [ -d "packages/$i/storybook" ] && rsync -aR packages/$i/storybook/ packages/server/public/pkgs/$name/storybook/
+  mkdir -p packages/server/public/pkgs/$name/{docs,storybook}
+  [ -d "packages/$i/docs" ] && rsync -a packages/$i/docs/ packages/server/public/pkgs/$name/docs/
+  [ -d "packages/$i/storybook" ] && rsync -a packages/$i/storybook/ packages/server/public/pkgs/$name/storybook/
 done
