4d4717aef0386b670690d2076c8ff03163d67a34
wener <wenermail@gmail.com>
2020-12-10 16:20:42 +0800

    useInterval allowed 0 interval to disable

--- packages/reaction/src/hooks/useInterval.ts
+++ packages/reaction/src/hooks/useInterval.ts
@@ -2,10 +2,10 @@ import { useEffect, useRef } from 'react';
 
 export function useInterval(handler: TimerHandler, interval: number) {
   const ref = useRef<any>();
-  // const [count, setCount] = useState(0);
-  // useEffect(handler, [count]);
   useEffect(() => {
-    // ref.current = setInterval(() => setCount(v => v + 1), interval);
+    if (!interval || interval < 0) {
+      return;
+    }
     ref.current = setInterval(handler, interval);
     return () => clearInterval(ref.current);
   }, [interval]);
